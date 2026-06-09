93d22ab63f62fe211cb3f327b4622c99347acbc2
Minseo Shin <minseo9451@gmail.com>
2026-06-09 09:59:30 +0900

    fix: GlobalExceptionHandler 범위를 자체 컨트롤러 패키지로 제한 (Swagger 500 오류 수정)

--- src/main/java/com/shoppingmall/goods/exception/GlobalExceptionHandler.java
+++ src/main/java/com/shoppingmall/goods/exception/GlobalExceptionHandler.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.stream.Collectors;
 
-@RestControllerAdvice
+@RestControllerAdvice(basePackages = "com.shoppingmall.goods.controller")
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(AuthException.class)
